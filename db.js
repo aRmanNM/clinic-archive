39b7ec8b3445cb6ed79776ed56c6f6d072dc89d6
armannm <anm4500@gmail.com>
2024-12-24 20:48:36 +0330

    change db address

--- db.js
+++ db.js
@@ -1,12 +1,9 @@
 const Database = require("better-sqlite3")
 const path = require("path")
 
-const dbPath =
-    process.env.NODE_ENV === "development"
-        ? "./patients.db"
-        : path.join(process.resourcesPath, "./patients.db")
+const dbPath = "Z:\patients.db"
 
 const db = new Database(dbPath)
-db.pragma("journal_mode = WAL")
+// db.pragma("journal_mode = WAL")
 
 exports.db = db
